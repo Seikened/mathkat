5c2c0ff6daa33b9f03994ff2a1d2b4408f8d5876
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-04-27 16:34:58 -0600

    Update version in setup.py to 1.1.0 and add tag.sh script for version tagging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mathkat',
-    version='1.0.0',
+    version='1.1.0',
     description='Una librería de gradientes para Python con impresión bonita en terminal.',
     author='Fernando Leon Franco',
     packages=find_packages(),
---
+++ tag.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Define la versión automáticamente (puedes ajustarla o obtenerla de otra fuente)
+VERSION="v1.4.0"
+
+echo "Creando una nueva etiqueta: $VERSION"
+git tag "$VERSION"
+
+echo "Subiendo la etiqueta al repositorio remoto"
+git push origin "$VERSION"
